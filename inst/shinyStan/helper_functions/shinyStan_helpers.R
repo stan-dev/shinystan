df3b9e27152524f94214d76a549f64a0366ea91f
jgabry <jgabry@gmail.com>
2015-03-08 16:19:01 -0400

    license

--- inst/shinyStan/helper_functions/shinyStan_helpers.R
+++ inst/shinyStan/helper_functions/shinyStan_helpers.R
@@ -1,3 +1,21 @@
+# shinyStan
+# Copyright (c) 2015 Jonah Gabry & shinyStan team
+# All rights reserved.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # ggplot theme elements --------------------------------------------
 theme <- function(...) ggplot2::theme(...)
 axis_line_color <- "#346fa1"
