30e5ef779cfec786c9787f7066eddf19cc4ede5d
jgabry <jgabry@gmail.com>
2015-03-09 16:15:05 -0400

    .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
-# .onAttach <- function(...) {
-#   lib <- dirname(system.file(package = "shinyStan"))
-#   des <- packageDescription("shinyStan", lib.loc = lib)
-#   version <- des$Version
-#   msg <- paste0("shinyStan v", version)
-#   packageStartupMessage(msg)
-# } 
-# 
+.onAttach <- function(...) {
+  lib <- dirname(system.file(package = "shinyStan"))
+  des <- packageDescription("shinyStan", lib.loc = lib)
+  version <- des$Version
+  msg <- paste0("shinyStan v", version)
+  packageStartupMessage(msg)
+} 
+
