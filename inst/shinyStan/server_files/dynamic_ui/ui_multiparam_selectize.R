d0dc2e552c2241b0f66a95d1ce746ad07ebd71ed
jgabry <jgabry@gmail.com>
2015-03-12 13:34:32 -0400

    add switch-style toggle buttons for show/hide customization

--- inst/shinyStan/server_files/dynamic_ui/ui_multiparam_selectize.R
+++ inst/shinyStan/server_files/dynamic_ui/ui_multiparam_selectize.R
@@ -1,7 +1,7 @@
 output$ui_multiparam_selectize <- renderUI({
   choices <- make_param_list_with_groups_sort()
   selected <- c(input$params_to_plot)
-  column(7, selectizeInput("params_to_plot",
+  column(6, selectizeInput("params_to_plot",
                            label = h5("Select or enter parameter names"),
                            width = '100%',
                            choices = choices,
