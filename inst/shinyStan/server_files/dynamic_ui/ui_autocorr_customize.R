b75f7c5812887e119313d1b4385f0e3190178f18
jgabry <jgabry@gmail.com>
2015-03-12 17:55:44 -0400

    default to inc_warmup=FALSE for autocorr

--- inst/shinyStan/server_files/dynamic_ui/ui_autocorr_customize.R
+++ inst/shinyStan/server_files/dynamic_ui/ui_autocorr_customize.R
@@ -13,7 +13,7 @@ output$ui_autocorr_customize <- renderUI({
     select_collapse <- bsCollapsePanel(title = "Options", id = "ac_options_collapse",
                                        sliderInput("ac_lags", label = "Lags", post = " lags", min = 0, max = nIter-warmup_val-5, step = 5, value = min(25, round((nIter-warmup_val)/2))),
                                        checkboxInput("ac_partial", "Partial autocorrelation", value = FALSE),
-                                       checkboxInput("ac_warmup", label = "Include warmup", TRUE),
+                                       checkboxInput("ac_warmup", label = "Include warmup", FALSE),
                                        checkboxInput("ac_combine", label = "Combine chains", FALSE)
 
     )
