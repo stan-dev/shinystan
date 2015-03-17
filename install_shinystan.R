bc549e254aff9fa95f06854202f9ad9e8dbcaa21
jgabry <jgabry@gmail.com>
2015-03-17 19:09:20 -0400

    update install_shinystan to old shinyBS ref
    [ci skip]

--- install_shinystan.R
+++ install_shinystan.R
@@ -33,7 +33,7 @@ install_shinystan <- function() {
   }
   
   install.packages(c("knitr", "htmlwidgets", "maps"), dependencies = TRUE)
-  devtools::install_github("ebailey78/shinyBS", ref = "shinyBS3", dependencies = TRUE)
+  devtools::install_github("ebailey78/shinyBS", ref = "f56e41b236ecda63c28af2ffd34b7b72a76c5ec5", dependencies = TRUE)
   devtools::install_github("bwlewis/rthreejs", dependencies = TRUE)
   devtools::install_github("stan-dev/shinyStan", build_vignettes = TRUE)
   
