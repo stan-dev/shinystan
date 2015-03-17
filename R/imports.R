4639dfa4d9e3d2a29470509711c6362d5d1006bd
jgabry <jgabry@gmail.com>
2015-03-17 08:52:12 -0400

    imports

---
+++ R/imports.R
@@ -0,0 +1,12 @@
+#' @import dygraphs
+#' @import markdown
+#' @import threejs
+#' @import xtable
+#' @import xts
+#' @import methods
+#' @importFrom gridExtra arrangeGrob grid.arrange
+#' @importFrom plyr ddply summarise
+#' @importFrom reshape2 melt
+#' @importFrom gtools mixedsort
+#' 
+NULL
