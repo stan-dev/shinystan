44faad0cd8b19238eb4c8676a44bb8f106b92f13
jgabry <jgabry@gmail.com>
2015-03-18 15:22:13 -0400

    tex modal
    [ci skip]

--- inst/shinyStan/server_files/dynamic_ui/ui_tex_modal.R
+++ inst/shinyStan/server_files/dynamic_ui/ui_tex_modal.R
@@ -1,6 +1,6 @@
 output$ui_tex_modal <- renderUI({
   bsModal("tex", title = "Print LaTeX table", trigger = "tex_options",
-          helpText("The LaTeX table will print in the R console and can be pasted into a .tex file"),
+          helpText(style = "color: #337ab7;","The LaTeX table will print in the R console and can be pasted into a .tex file"),
           selectizeInput("tex_params", width = "100%", label = h5("Select or enter parameter names"), choices = .make_param_list_with_groups(object), multiple = TRUE,
                          options = list(placeholder = "Leave blank for all parameters")),
           flowLayout(checkboxInput("tex_booktabs", strong("Booktabs"), value = TRUE),
@@ -8,7 +8,7 @@ output$ui_tex_modal <- renderUI({
           flowLayout(checkboxInput("tex_long", strong("Longtable"), value = FALSE),
                      helpText("Use LaTeX package 'longtable' for tables longer than a single page.")),
           hr(),
-          actionButton("tex_go", "Print LaTeX")
+          actionButton("tex_go", "Print LaTeX", icon = icon("print", lib = "glyphicon"))
   )
 })
 
