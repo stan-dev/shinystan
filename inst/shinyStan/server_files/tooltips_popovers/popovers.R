8913afab0f3f1b10c5cd47a26ea7091ab36205fb
jgabry <jgabry@gmail.com>
2015-03-18 15:23:23 -0400

    cleanup old tooltips and popovers code
    [ci skip]

--- inst/shinyStan/server_files/tooltips_popovers/popovers.R
+++
@@ -1,3 +0,0 @@
-# popover_ids <- c(paste0("tex_", c("booktabs", "long")))
-# popover_msgs <- c("From print.xtable {xtable}: If TRUE, the toprule, midrule and bottomrule tags from the LaTex 'booktabs' package are used rather than hline for the horizontal line tags.",
-#                   "For tables longer than a single page. If TRUE, will use LaTeX package 'longtable'.")
