40ff36f6f8663c16d73f3e2aa2c11aab21ca57e1
jgabry <jgabry@gmail.com>
2015-03-24 09:36:05 -0400

    update install_shinystan
    [ci skip]

--- install_shinystan.R
+++ install_shinystan.R
@@ -12,10 +12,10 @@
 #######################################################
 
 install_shinystan <- function() {
-
+  
   if (getRversion() < '3.1.2') stop("shinyStan requires R version 3.1.2 or greater.")
   
-  msg <- "Note: this will install or update any missing or out-of-date packages needed to run shinyStan. Do you want to continue?"
+  msg <- "Note: this will install or update packages needed to run shinyStan. Do you want to continue?"
   continue <- select.list(choices = c("Yes", "No"), title = msg, graphics = FALSE)
   if (continue == "No") {
     message("Installation canceled by user.")
@@ -32,7 +32,7 @@ install_shinystan <- function() {
   } 
   
   # install needed packages from CRAN
-  install.packages(c("shiny", "knitr", "htmlwidgets", "maps"), dependencies = TRUE)
+  install.packages(c("shiny", "knitr", "markdown", "htmlwidgets", "maps"), dependencies = TRUE)
   
   # install needed packages from GitHub
   devtools::install_github("jgabry/shinyBS", ref = "shinyBS_for_shinyStan")
