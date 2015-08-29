ab6cba7f73e3ba1f792025bce9d24430066ed084
jgabry <jgabry@gmail.com>
2015-08-29 00:14:53 -0400

    fix stanreg param_dims issue

--- R/stanreg2shinystan.R
+++ R/stanreg2shinystan.R
@@ -6,6 +6,16 @@ stanreg2shinystan <- function(X, ...) {
     return(invisible(NULL))
   }
   sso <- stan2shinystan(X$stanfit, ...)
+  
+  param_names <- sso@param_names
+  param_dims <- list()
+  param_dims[1:length(param_names)] <- NA
+  names(param_dims) <- param_names
+  for(i in 1:length(param_names)) {
+    param_dims[[i]] <- numeric(0)
+  }
+  sso@param_dims <- param_dims
+  
   sso@misc$pp_y <- if ("y" %in% names(X)) 
     X$y else model.response(model.frame(X))
   sso@misc$pp_yrep <- rstanarm::posterior_predict(X)
