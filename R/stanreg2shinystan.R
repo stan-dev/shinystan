078700532c274ef20d08bd04aff8c70d786365d4
jgabry <jgabry@gmail.com>
2015-09-11 10:49:44 -0400

    rstanarm fixes

--- R/stanreg2shinystan.R
+++ R/stanreg2shinystan.R
@@ -1,12 +1,6 @@
 stanreg2shinystan <- function(X, ...) {
   stopifnot(is.stanreg(X))
-  ok <- requireNamespace("rstanarm", quietly = TRUE)
-  if (!ok) {
-    message("Please install the rstanarm package to use this feature.")
-    return(invisible(NULL))
-  }
   sso <- stan2shinystan(X$stanfit, ...)
-  
   param_names <- sso@param_names
   param_dims <- list()
   param_dims[1:length(param_names)] <- NA
@@ -15,9 +9,10 @@ stanreg2shinystan <- function(X, ...) {
     param_dims[[i]] <- numeric(0)
   }
   sso@param_dims <- param_dims
-  
+  posterior_predict <- 
+    getFromNamespace("posterior_predict", "rstanarm")
   sso@misc$pp_y <- if ("y" %in% names(X)) 
     X$y else model.response(model.frame(X))
-  sso@misc$pp_yrep <- rstanarm::posterior_predict(X)
+  sso@misc$pp_yrep <- posterior_predict(X)
   sso
 }
