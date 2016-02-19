531dfe25fdaa40a458a812dc7e9f2d6ef6241c4a
jgabry <jgabry@gmail.com>
2016-02-19 18:45:44 -0500

    minor doc tweaks

--- R/launch_shinystan.R
+++ R/launch_shinystan.R
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <http://www.gnu.org/licenses/>.
 
-#' ShinyStan app
+#' Launch the ShinyStan app
 #' 
 #' @export
 #' @param object An object of class shinystan, stanfit, or stanreg. See
