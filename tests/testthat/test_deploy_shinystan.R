107f7ca94f37f3dc994b300b266c24cfbbaf5fe2
jgabry <jgabry@gmail.com>
2016-05-02 00:38:58 -0400

    test deploy_shinystan internals

--- tests/testthat/test_deploy_shinystan.R
+++ tests/testthat/test_deploy_shinystan.R
@@ -15,3 +15,15 @@ test_that("deploy_shinystan preprocessing doesn't error", {
   expect_true(grepl("shinystan", deploy_test, ignore.case = TRUE))
   expect_true(dir.exists(deploy_test))
 })
+
+test_that("deploy_shinystan pp_check processing functions ok", {
+  x <- "123454321"
+  expect_output(cat(.y_lines(x)), x)
+  expect_output(cat(.yrep_lines(x)), x)
+  
+  deploy_pp_test <- deploy_shinystan(sso, appName = "test", deploy = FALSE, 
+                                     ppcheck_data = rep(1, 8), 
+                                     ppcheck_yrep = "yrep")
+  expect_true(dir.exists(deploy_pp_test))
+})
+
