9c22f62ac0dd43554971e8779e4b4f76a4bcf8f3
bob_kubinec <rmk7xy@virginia.edu>
2016-06-12 13:17:05 +0100

    create basic plot function for animate in animate.R

---
+++ inst/ShinyStan/server_files/pages/explore/server/animate.R
@@ -0,0 +1,78 @@
+# # animate scatterplot
+#Transforms would be nice, but let's start small
+
+# bivariate_transform_x <-
+#   eventReactive(input$bivariate_transform_go > 0, input$bivariate_transform_x)
+# bivariate_transform_y <-
+#   eventReactive(input$bivariate_transform_go > 0, input$bivariate_transform_y)
+bivariate_plot <- reactive({
+  validate(
+    need(input$param, message = FALSE),
+#    need(input$bivariate_ellipse_lev, message = FALSE),
+    need(input$animate_param_y, message = FALSE)
+  )
+  
+#   if (!is.null(input$bivariate_ellipse_lev)) {
+#     validate(
+#       need(is.numeric(input$bivariate_pt_size), message = "Point size must be numeric"),
+#       need(is.numeric(input$bivariate_pt_shape), message = "Point shape must be numeric")
+#     )
+#     
+#     if (input$bivariate_ellipse_lev != "None") {
+#       validate(
+#         need(
+#           input$param != input$bivariate_param_y,
+#           "For this option the x and y can't be the same parameter."
+#         ),
+#         need(
+#           is.numeric(input$bivariate_ellipse_lwd),
+#           message = "Ellipse size must be numeric"
+#         ),
+#         need(
+#           is.numeric(input$bivariate_ellipse_lty),
+#           message = "Ellipse shape must be numeric"
+#         )
+#       )
+#     }
+#   }
+  
+  do.call(
+    ".animate_plot",
+    args = list(
+      samps = SAMPS_post_warmup,
+      sp = if (!identical(SAMPLER_PARAMS_post_warmup, FALSE)) 
+        SAMPLER_PARAMS_post_warmup else NULL,
+      max_td = if ("max_td" %in% names(MISC)) MISC$max_td else NULL,
+      param = input$animate_param_y,
+      param2 = input$animate_param_x
+#       pt_alpha = input$bivariate_pt_alpha,
+#       pt_size = input$bivariate_pt_size,
+#       pt_shape = input$bivariate_pt_shape,
+#       pt_color = input$bivariate_pt_color,
+#       ellipse_lev = input$bivariate_ellipse_lev,
+#       ellipse_color = input$bivariate_ellipse_color,
+#       ellipse_lty = input$bivariate_ellipse_lty,
+#       ellipse_lwd = input$bivariate_ellipse_lwd,
+#       ellipse_alpha = input$bivariate_ellipse_alpha,
+#       lines  = input$bivariate_lines,
+#       lines_color = input$bivariate_lines_color,
+#       lines_alpha = input$bivariate_lines_alpha,
+#       transform_x = bivariate_transform_x(),
+#       transform_y = bivariate_transform_y()
+    )
+  )
+})
+
+output$animate_plot_out <- renderImage({
+  # Return a list with a src attribute that equals the location of the GIF file
+  animate_plot()
+})
+
+# download
+# output$download_animate <- downloadHandler(
+#   filename = 'shinystan-bivariate-gg.RData',
+#   content = function(file) {
+#     shinystan_bivariate_gg <- bivariate_plot()
+#     save(shinystan_bivariate_gg, file = file)
+#   }
+# )
