937ecd481cce15a382e7cf272187121fe92c3e8a
jgabry <jgabry@gmail.com>
2016-08-29 16:11:24 -0400

    remove deprecated copySWF

--- inst/ShinyStan/server_files/pages/estimate/server/summary_stats.R
+++ inst/ShinyStan/server_files/pages/estimate/server/summary_stats.R
@@ -1,14 +1,19 @@
 summary_stats <- reactive({
   `%>%` <- DT::`%>%`
   validate(need(input$table_digits, "loading"))
-  DT::datatable(data = round(TABLE_STATS, digits = input$table_digits), 
+  DT::datatable(data = round(TABLE_STATS, digits = input$table_digits),
                 colnames = c('mcse' = 'se_mean'),
                 options = list(
                   colReorder = list(realtime = TRUE),
-                  dom = 'RfClrTtip',
-                  colVis = list(exclude = list(0), activate = 'mouseover'),
-                  pageLength = 10, 
-                  # autoWidth = TRUE,
+                  # dom = 'RBfClrTtip',
+                  dom = "Bflrtip",
+                  buttons = list('copy', 'print', list(
+                    extend = 'collection',
+                    buttons = c('csv', 'pdf'),
+                    text = 'Download'
+                  ), 
+                  list(extend = 'colvis', columns = 1:ncol(TABLE_STATS))),
+                  pageLength = 10,
                   pagingType = "full",
                   processing = TRUE,
                   deferRender = TRUE,
@@ -16,26 +21,14 @@ summary_stats <- reactive({
                   scrollX = TRUE,
                   scrollCollapse = FALSE,
                   language = list(search = "_INPUT_", searchPlaceholder = "Regex searching"),
-                  #       preDrawCallback = DT::JS('function() { 
-                  # Shiny.unbindAll(this.api().table().node()); }'), 
-                  #       drawCallback = DT::JS('function() { 
-                  # Shiny.bindAll(this.api().table().node()); } '),
-                  search = list(regex = TRUE),
-                  # searchHighlight = TRUE,
-                  tableTools = list(sSwfPath = DT::copySWF("www", pdf = TRUE), 
-                                    aButtons = list('copy', 'print', list(
-                                      sExtends = 'collection',
-                                      sButtonText = 'Save',
-                                      aButtons = c('csv', 'pdf')
-                                    )))
-                ), 
-                # filter = list(position = 'top', clear = TRUE, plain = FALSE),
-                extensions = 
-                  c("TableTools", "ColReorder", "ColVis", "FixedColumns", "Scroller")) %>%
-    DT::formatStyle(columns = "Rhat", 
-                    color = DT::styleInterval(1.1, c("blue", "red"))) %>% 
+                  search = list(regex = TRUE)
+                ),
+                extensions =
+                  c("Buttons", "ColReorder", "FixedColumns", "Scroller")) %>%
+    DT::formatStyle(columns = "Rhat",
+                    color = DT::styleInterval(1.1, c("blue", "red"))) %>%
     DT::formatRound(columns = "n_eff", digits = 0) # %>%
-  #     DT::formatRound(columns = c("Rhat", "mean", "mcse", "sd", "2.5%", "25%", 
+  #     DT::formatRound(columns = c("Rhat", "mean", "mcse", "sd", "2.5%", "25%",
   #                                 "50%", "75%", "97.5%"), digits = input$table_digits)
 })
 output$all_summary_out <- DT::renderDataTable({
