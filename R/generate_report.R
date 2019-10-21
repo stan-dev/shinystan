71732b018c0445833acb15b489ef95b6f4e3bdc4
VeenDuco <ducoveen@gmail.com>
2019-10-21 17:57:40 +0200

    add first setup for full automated report with function
    [ci skip]

---
+++ R/generate_report.R
@@ -0,0 +1,14 @@
+generate_report <- function (sso, n_param = 1, output_format = "html_document", view = TRUE) {
+  if(class(sso) != "shinystan") stop("Object is not of class 'shinystan'.")
+  path <- rmarkdown::render(input = system.file("ShinyStanModules/reports/report_function.Rmd",
+                                                package = "shinystan"), 
+                            output_format = output_format, output_dir = getwd())
+  message("File saved to ", path)
+  if (view) {
+    system2("open", shQuote(path))
+  }
+}
+
+
+
+
