ebc5ac7e16dd2fa5c08cccab84dfbbbc9cb09d01
Duco Veen <ducoveen@gmail.com>
2020-01-06 14:13:07 +0100

    Fix loading error some OS
    some operating systems require case sensitive extensions, fix that they are correct now.
    
    [ci skip]

--- inst/ShinyStanModules/server.r
+++ inst/ShinyStanModules/server.r
@@ -1,14 +1,14 @@
 server <- function(input, output, session) {
   # utilities
-  source("MODULES/UTILS/plotOptions.r", local = TRUE)
-  source("MODULES/UTILS/report.r", local = TRUE)
+  source("MODULES/UTILS/plotOptions.R", local = TRUE)
+  source("MODULES/UTILS/report.R", local = TRUE)
   
   # home tab
-  source("MODULES/HOME/homepage.r", local = TRUE)
+  source("MODULES/HOME/homepage.R", local = TRUE)
   source("MODULES/HOME/warnings.r", local = TRUE)
   
   # diagnoses tab
-  source("MODULES/DIAGNOSE/diagnoseHomepage.r", local = TRUE)
+  source("MODULES/DIAGNOSE/diagnoseHomepage.R", local = TRUE)
   
   source("MODULES/DIAGNOSE/divergentScatter.r", local = TRUE)
   source("MODULES/DIAGNOSE/divergentTransitions.r", local = TRUE)
@@ -28,23 +28,23 @@ server <- function(input, output, session) {
   source("MODULES/DIAGNOSE/rhat_n_eff_se_mean_stats.r", local = TRUE)
   
   # estimate tab
-  source("MODULES/ESTIMATE/estimateHomepage.r", local = TRUE)
+  source("MODULES/ESTIMATE/estimateHomepage.R", local = TRUE)
   
-  source("MODULES/ESTIMATE/visualEstimate.r", local = TRUE)
-  source("MODULES/ESTIMATE/scatterPlot.r", local = TRUE)
-  source("MODULES/ESTIMATE/densityPlot.r", local = TRUE)
-  source("MODULES/ESTIMATE/histogramPlot.r", local = TRUE)
-  source("MODULES/ESTIMATE/intervalsPlot.r", local = TRUE)
-  source("MODULES/ESTIMATE/areasPlot.r", local = TRUE)
+  source("MODULES/ESTIMATE/visualEstimate.R", local = TRUE)
+  source("MODULES/ESTIMATE/scatterPlot.R", local = TRUE)
+  source("MODULES/ESTIMATE/densityPlot.R", local = TRUE)
+  source("MODULES/ESTIMATE/histogramPlot.R", local = TRUE)
+  source("MODULES/ESTIMATE/intervalsPlot.R", local = TRUE)
+  source("MODULES/ESTIMATE/areasPlot.R", local = TRUE)
   
-  source("MODULES/ESTIMATE/numericalEstimate.r", local = TRUE)
-  source("MODULES/ESTIMATE/summaryTable.r", local = TRUE)
-  source("MODULES/ESTIMATE/summaryTableLatex.r", local = TRUE)
+  source("MODULES/ESTIMATE/numericalEstimate.R", local = TRUE)
+  source("MODULES/ESTIMATE/summaryTable.R", local = TRUE)
+  source("MODULES/ESTIMATE/summaryTableLatex.R", local = TRUE)
   
   # more tab
-  source("MODULES/MORE/about.r", local = TRUE)
-  source("MODULES/MORE/modelCode.r", local = TRUE)
-  source("MODULES/MORE/help.r", local = TRUE)
+  source("MODULES/MORE/about.R", local = TRUE)
+  source("MODULES/MORE/modelCode.R", local = TRUE)
+  source("MODULES/MORE/help.R", local = TRUE)
 
   # internal functions or events that are required for general use.
   # save and close button
